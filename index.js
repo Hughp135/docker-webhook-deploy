06cf18b479a0c45a1e0bdc728bf9924a9357fa10
Hugh <hcmpercival@gmail.com>
2018-03-30 00:26:44 +0100

    initial

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var key = fs.readFileSync('key.pem');
+var cert = fs.readFileSync( 'cert.pem' );
+var options = {
+  key: key,
+  cert: cert,
+};
+
+app.post('/deploy', (req, res) => {
+  console.log(req.body);
+  res.send('success');
+  exec("cd ../ && docker-compose up -d && docker system prune -f", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+});
+
+var https = require('https');
+https.createServer(options, app).listen(957);
+
+console.log('Server listening on 957');
